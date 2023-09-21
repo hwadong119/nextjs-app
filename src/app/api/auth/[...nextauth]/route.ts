205abf5684160b15de7aea56ebd2f4dcf132c503
hwadong119 <jeonghwayun0119@naver.com>
2023-09-21 15:53:08 +0900

    권한에 따른 인가 처리하기(authorization)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -28,7 +28,7 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com', role: 'Admin' }
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
@@ -45,6 +45,10 @@ const handler = NextAuth({
   session: {
     strategy: 'jwt',
   }, 
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    maxAge: 30 * 24 * 60 * 60 // 30days
+  },
   callbacks: {
     async jwt({ token, user }) {
       return {...token, ...user }
