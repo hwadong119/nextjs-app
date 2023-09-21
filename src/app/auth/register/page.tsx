d64ce2394d2fb160b4ff08b570e1a68721c964da
hwadong119 <jeonghwayun0119@naver.com>
2023-09-21 17:09:10 +0900

    로그인, 회원가입 UI 생성하기

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -7,15 +7,15 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import axios from 'axios';
 import { Router } from 'next/router'
 import { useRouter } from 'next/navigation'
-import { signIn } from 'next-auth/react'
 
-const LoginPage = () => {
+const RegisterPage = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
     defaultValues: {
+      name: '',
       email: '',
       password: ''
     }
@@ -24,7 +24,7 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (body) => {
     setIsLoading(true);
     try {
-      const data = signIn('credentials', body);
+      const { data } = await axios.post('/api/register', body);
       console.log(data);
       router.push('/auth/login');
     } catch (error) {
@@ -40,10 +40,18 @@ const LoginPage = () => {
         onSubmit={handleSubmit(onSubmit)}
         className='flex flex-col justify-center gap-4 min-w-[350px]'
       >
-        <h1 className='text-2xl'>Login</h1>
+        <h1 className='text-2xl'>Register</h1>
         <Input
           id='email' 
           label='Email'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id='name' 
+          label='Name'
           disabled={isLoading}
           register={register}
           errors={errors}
@@ -65,9 +73,9 @@ const LoginPage = () => {
 
         <div className='text-center'>
           <p className='text-gray-400'>
-            Not a member?{' '}
-            <Link href='/auth/register' className='text-black hover:underline'>
-              Register
+            Already a member?{' '}
+            <Link href='/auth/login' className='text-black hover:underline'>
+              Login
             </Link>
           </p>
         </div>
@@ -77,4 +85,4 @@ const LoginPage = () => {
   )
 }
 
-export default LoginPage
+export default RegisterPage
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -38,6 +38,7 @@ const Input: React.FC<InputProps> = ({
           p-4
           pt-6
           font-light
+          border-2
           bg-white
           rounded-md
           outline-none
