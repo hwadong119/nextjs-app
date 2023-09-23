7b1714150ecc07f538830c2bba006c019d4a40b8
hwadong119 <jeonghwayun0119@naver.com>
2023-09-23 13:48:08 +0900

    Pagination 기능 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,12 +7,17 @@ import ProductCard from '@/components/ProductCard'
 import getCurrentUser from '../actions/getCurrentUser'
 import FloatingButton from '@/components/FloatingButton'
 import Categories from '@/components/categories/Categories'
+import Pagination from '@/components/Pagination'
+import { PRODUCTS_PER_PAGE } from '@/constants'
 
 interface HomeProps {
   searchParams: ProductsParams
 }
 
 export default async function Home({searchParams}: HomeProps) {
+
+  const page = searchParams?.page;
+  const pageNum = typeof page === 'string' ? Number(page) : 1;
 
   const products = await getProducts(searchParams);
   const currentUser = await getCurrentUser();
@@ -50,6 +55,8 @@ export default async function Home({searchParams}: HomeProps) {
             </div> 
           </>
       }
+
+      <Pagination page={pageNum} totalItems={products.totalItems} perPage={PRODUCTS_PER_PAGE} />
 
       <FloatingButton
         href='/products/upload'
