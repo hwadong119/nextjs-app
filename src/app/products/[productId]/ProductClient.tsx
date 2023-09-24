7c658eddbc2359681bd1a77661235ee9eca55f29
hwadong119 <jeonghwayun0119@naver.com>
2023-09-24 16:28:40 +0900

    ProductInfo 컴포넌트 생성

--- src/app/products/[productId]/ProductClient.tsx
+++ src/app/products/[productId]/ProductClient.tsx
@@ -3,6 +3,7 @@
 'use client'
 import Button from '@/components/Button';
 import Container from '@/components/Container';
+import { categories } from '@/components/categories/Categories';
 import ProductHead from '@/components/products/ProductHead';
 import ProductInfo from '@/components/products/ProductInfo';
 import { Product, User } from '@prisma/client'
@@ -22,6 +23,8 @@ const ProductClient = ({product, currentUser}: ProductClientProps) => {
     ssr: false
   })
 
+  const category = categories.find((item) => item.path === product.category)
+
   return (
     <Container>
       <div className='max-w-screen-lg mx-auto'>
@@ -33,7 +36,12 @@ const ProductClient = ({product, currentUser}: ProductClientProps) => {
             currentUser={currentUser}
           />
           <div className='grid grid-cols-1 mt-6 md:grid-cols-2 md:gap-10'>
-            <ProductInfo />
+            <ProductInfo 
+              user={product.user}
+              category={category}
+              createdAt={product.createdAt}
+              description={product.description}
+            />
             <div>
               <KakaoMap 
                 detailPage 
