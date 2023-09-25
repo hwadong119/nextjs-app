1d4ac22314c0e4427b86f6240a68257425ff739e
hwadong119 <jeonghwayun0119@naver.com>
2023-09-25 14:31:33 +0900

    ChatHeader 컴포넌트 생성

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,45 +1,52 @@
-import { TUserWithChat } from '@/types'
-import React from 'react'
-import Input from './Input';
+import { TUserWithChat } from "@/types";
+import React from "react";
+import Input from "./Input";
+import ChatHeader from "./ChatHeader";
 
 interface ChatProps {
   currentUser: TUserWithChat;
   receiver: {
-    receiverId: string,
-    receiverName: string,
-    receiverImage: string
+    receiverId: string;
+    receiverName: string;
+    receiverImage: string;
   };
   setLayout: (layout: boolean) => void;
 }
 
-const Chat = ({
-  currentUser,
-  receiver,
-  setLayout
-}: ChatProps) => {
+const Chat = ({ currentUser, receiver, setLayout }: ChatProps) => {
+  const conversation = 
+    currentUser?.conversations.find((conversation) => 
+      conversation.users.find((user) => user.id === receiver.receiverId))
 
-  if(!receiver.receiverName || !currentUser) {
-    return <div className='w-full h-full'></div>
+  if (!receiver.receiverName || !currentUser) {
+    return <div className="w-full h-full"></div>;
   }
-  
+
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <div>
-        {/* {chat header} */}
+        <ChatHeader
+          setLayout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={
+            conversation?.messages
+              .filter(message => message.receiverId === currentUser.id)
+              .slice(-1)[0]?.createdAt
+          }
+        />
       </div>
 
-      <div className='flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]'>
-
-      </div>
+      <div className="flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]"></div>
 
       <div>
-        <Input 
+        <Input
           receiverId={receiver?.receiverId}
           currentUserId={currentUser?.id}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
